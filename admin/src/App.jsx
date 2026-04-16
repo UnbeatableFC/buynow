3f4a87a4e5f33f47c64819a1a6cdba4960597e42
Wisdom Mpamugo <wisdommpam99@gmail.com>
2026-04-16 10:07:45 +0100

    Dashboard Page Added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,19 +1,44 @@
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
-import React from 'react'
+import { Navigate, Route, Routes } from "react-router";
+import LoginPage from "./pages/LoginPage.jsx";
+import { useAuth } from "@clerk/react";
+import DashboardPage from "./pages/DashboardPage.jsx";
+import ProductsPage from "./pages/ProductsPage.jsx";
+import OrdersPage from "./pages/OrdersPage.jsx";
+import CustomersPage from "./pages/CustomersPage.jsx";
+import DashboardLayout from "./layouts/DashboardLayout.js";
+import PageLoader from "./components/PageLoader.jsx";
 
 const App = () => {
-  return (
-    <div>
-      <h1>HOME PAGE</h1>
-        <Show when="signed-out">
-          <SignInButton mode='modal'/>
-          <SignUpButton mode='modal' />
-        </Show>
-        <Show when="signed-in">
-          <UserButton/>
-        </Show>
-    </div>
-  )
-}
+  const { isSignedIn, isLoaded } = useAuth();
 
-export default App
+  if (!isLoaded) return <PageLoader />;
+
+  return (
+    <Routes>
+      <Route
+        path="/login"
+        element={
+          isSignedIn ? <Navigate to={"/dashboard"} /> : <LoginPage />
+        }
+      />
+      <Route
+        path="/"
+        element={
+          isSignedIn ? (
+            <DashboardLayout />
+          ) : (
+            <Navigate to={"/login"} />
+          )
+        }
+      >
+        <Route index element={<Navigate to={"dashboard"} />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="products" element={<ProductsPage />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="customers" element={<CustomersPage />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
---
+++ admin/src/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+import { SignIn } from "@clerk/react";
+
+function LoginPage() {
+  return (
+    <div className="h-screen hero">
+      <SignIn />
+    </div>
+  );
+}
+export default LoginPage;
